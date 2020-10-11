473ba3c829fcdbeaa9da5f6dd08b339939c0da23
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-10-11 12:26:50 +0530

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Layout({title}) {
+    return (
+        <div className="bg-gray-300">
+<Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="container mx-auto max-w-xl pt-8 min-h-screen"></main>
+        </div>
+    )
+}
